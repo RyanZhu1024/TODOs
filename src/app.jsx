a9f6f1ec71c1f9b0bf318ce19df65bb8cfd346df
RyanZhu <troyzx1024@gmail.com>
2016-02-29 22:28:25 -0800

    initialized

--- src/app.jsx
+++ src/app.jsx
@@ -1,13 +1,21 @@
 var React = require('react');
 var ReactDOM = require('react-dom');
+var ReactFire = require('reactfire');
+var Firebase = require('firebase');
+var rootUrl = 'https://amber-fire-6772.firebaseio.com/';
 
-var Hello = React.createClass({
-  render: function() {
-    return <h1 className="red">
-      Hello!
-    </h1>
-  }
+var App = React.createClass({
+    mixins: [ReactFire],
+    componentWillMount: function(){
+        this.bindAsObject(new Firebase(rootUrl + 'items/'),'item');
+    },
+    render: function() {
+        console.log(this.state);
+        return <h1 className="red">
+            App! React!
+        </h1>
+    }
 });
 
-var element = React.createElement(Hello, {});
+var element = React.createElement(App, {});
 ReactDOM.render(element, document.querySelector('.container'));
